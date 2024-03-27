a9ae3c7bf5eced40c263ad9bfdae6d198e49f6a7
kshartul <kumarshartul24@gmail.com>
2024-03-28 05:16:09 +0530

    deploy

--- eks/providers.tf
+++ eks/providers.tf
@@ -22,9 +22,6 @@ terraform {
 provider "aws" {
   region    = var.region
   profile   = "default"
-    assume_role {
-    role_arn = "arn:aws:iam:${data.aws_caller_identity.current.account_id}:role/tf-admin"
-  }
 }
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
