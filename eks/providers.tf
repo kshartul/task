4cce1eee797d107b4b5e4c49197f0d13b81b77eb
kshartul <kumarshartul24@gmail.com>
2024-03-28 05:13:56 +0530

    deploy

--- eks/providers.tf
+++ eks/providers.tf
@@ -23,7 +23,7 @@ provider "aws" {
   region    = var.region
   profile   = "default"
     assume_role {
-    role_arn = "arn:aws:iam:${data.aws_caller_identity.current.account_id}::role/tf-admin"
+    role_arn = "arn:aws:iam:${data.aws_caller_identity.current.account_id}:role/tf-admin"
   }
 }
 provider "kubernetes" {
