59bf9ccffb71430a517141a87e5b29dcea50771d
kshartul <kumarshartul24@gmail.com>
2024-03-28 05:45:45 +0530

    deploy

--- eks/eks.tf
+++ eks/eks.tf
@@ -119,6 +119,11 @@ resource "aws_eks_cluster" "kube_cluster" {
     endpoint_public_access  = var.public_endpoint_api
   }
 }
+resource "aws_cloudwatch_log_group" "log_groups_eks" {
+  name              = "/aws/eks/${var.environment}/cluster"
+  retention_in_days = var.cloudwatch_log_group_retention_in_days
+  kms_key_id        = var.kms_arn
+}
 
 ################################
 #####  EKS worker node role ####
