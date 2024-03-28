6fbe28f2038d207b3e8bce1bc9b857d04f9a13fb
kshartul <kumarshartul24@gmail.com>
2024-03-28 19:25:05 +0530

    add the files

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 module "dev_cluster" {
-  source = "eks"
+  source = "/eks"
 
   cluster_name = "dev"
 }
 
 module "staging_cluster" {
-  source = "eks"
+  source = "/eks"
 
   cluster_name = "staging"
 }
 
 module "production_cluster" {
-  source = "eks"
+  source = "/eks"
 
   cluster_name = "production"
 }
