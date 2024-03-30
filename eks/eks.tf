b4b094bc7c22942b030a19ad9a386e9a4528fb50
kshartul <kumarshartul24@gmail.com>
2024-03-30 20:13:46 +0530

    Karpenter changes

--- eks/eks.tf
+++ eks/eks.tf
@@ -49,15 +49,3 @@ module "eks" {
   
 }
 
-module "karpenter" {
-  source       = "terraform-aws-modules/eks/aws//modules/karpenter"
-  tags         = var.tags
-  cluster_name = module.eks.cluster_name
-
-  irsa_oidc_provider_arn       = module.eks.oidc_provider_arn
-  iam_role_additional_policies = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
-
-  policies = {
-    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  }
-}
--- eks/karpenter.tf
+++ eks/karpenter.tf
@@ -3,15 +3,16 @@ data "aws_ecrpublic_authorization_token" "token" {}
 module "karpenter" {
   source       = "terraform-aws-modules/eks/aws//modules/karpenter"
   tags         = var.tags
-  cluster_name = module.eks_cluster.cluster_name
+  cluster_name = module.eks.cluster_name
 
-  irsa_oidc_provider_arn       = module.eks_cluster.oidc_provider_arn
+  irsa_oidc_provider_arn       = module.eks.oidc_provider_arn
   iam_role_additional_policies = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
 
   policies = {
     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   }
 }
+
 
 resource "helm_release" "karpenter" {
   namespace        = "karpenter"
