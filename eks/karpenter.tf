762fbc49761345d17ca786f14855c1e81a440a97
kshartul <kumarshartul24@gmail.com>
2024-04-01 00:35:46 +0530

    Karpenter changes

--- eks/karpenter.tf
+++ eks/karpenter.tf
@@ -4,11 +4,9 @@ module "karpenter" {
   source       = "terraform-aws-modules/eks/aws//modules/karpenter"
   cluster_name = module.eks.cluster_name
 
-  create_node_iam_role = false
-  node_iam_role_arn    = module.eks.eks_managed_node_groups["initial"].iam_role_arn
-
-  # Since the nodegroup role will already have an access entry
-  create_access_entry = false
+  node_iam_role_additional_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
 
   tags = {
     Environment = "dev"
