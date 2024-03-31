03b983589e4e4dcf5c3f79df1baaeb878f307b84
kshartul <kumarshartul24@gmail.com>
2024-04-01 02:09:56 +0530

    Karpenter changes

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -49,6 +49,6 @@ output "karpenter_aws_node_instance_profile_name" {
 output "karpenter_sqs_queue_name" {
   value = module.karpenter.queue_name
 }
-output "karpenter_irsa_arn" {
-  value = module.karpenter.irsa_arn
-}
+#output "karpenter_irsa_arn" {
+#  value = module.karpenter.irsa_arn
+#}
