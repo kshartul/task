2456b56eb8528c827a0a8fac9b968e6df2219951
kshartul <kumarshartul24@gmail.com>
2024-04-01 00:49:29 +0530

    Karpenter changes

--- eks/karpenter.tf
+++ eks/karpenter.tf
@@ -4,10 +4,11 @@ module "karpenter" {
   source       = "terraform-aws-modules/eks/aws//modules/karpenter"
 
   cluster_name = module.eks.cluster_name
-
- node_iam_role_additional_policies = {
-    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  }
+  irsa_oidc_provider_arn          = module.eks.oidc_provider_arn
+  irsa_namespace_service_accounts = ["karpenter:karpenter"]
+  create_iam_role                 = false
+  iam_role_arn                    = module.eks.eks_managed_node_groups["initial"].iam_role_arn
+ 
 }
 
 
