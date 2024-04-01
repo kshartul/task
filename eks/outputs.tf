18cad78f9eec755f62df26be7cfa0dfa1393c49a
kshartul <kumarshartul24@gmail.com>
2024-04-01 11:28:56 +0530

    Karpenter changes

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -41,14 +41,3 @@ output "vpc_private_subnets" {
 output "vpc_intra_subnets" {
   value = [module.subnet_addrs.network_cidr_blocks["intra-1"], module.subnet_addrs.network_cidr_blocks["intra-2"]]
 }
-
-output "karpenter_aws_node_instance_profile_name" {
-  value = module.karpenter.instance_profile_name
-}
-
-output "karpenter_sqs_queue_name" {
-  value = module.karpenter.queue_name
-}
-#output "karpenter_irsa_arn" {
-#  value = module.karpenter.irsa_arn
-#}
